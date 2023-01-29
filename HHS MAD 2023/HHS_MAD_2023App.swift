8ee4117d2b227e9747c2c6b39b161e6b5f107f38
Julian NTH <jngthowhing627@gmail.com>
2023-01-28 17:31:18 -0800

    Initial Commit

---
+++ HHS MAD 2023/HHS_MAD_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  HHS_MAD_2023App.swift
+//  HHS MAD 2023
+//
+//  Created by Julian NTH on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct HHS_MAD_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HHS MAD 2023UITests/HHS_MAD_2023UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HHS_MAD_2023UITestsLaunchTests.swift
+//  HHS MAD 2023UITests
+//
+//  Created by Julian NTH on 1/28/23.
+//
+
+import XCTest
+
+final class HHS_MAD_2023UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
