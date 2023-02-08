edb75226f07e096e041d94a0400228605a79e82f
Julian NTH <jngthowhing627@gmail.com>
2023-02-07 21:40:52 -0800

    HomeScreen

---
+++ HHS MAD 2023/Preview Content/CalendarUI.swift
@@ -0,0 +1,26 @@
+//
+//  CalendarUI.swift
+//  HHS MAD 2023
+//
+//  Created by Julian NTH on 2/7/23.
+//
+
+import SwiftUI
+
+struct CalendarUI: View {
+    var body: some View {
+        HStack{
+            Button{
+                
+            } label: {
+                Image(systemName: "circle.fill")
+            }
+        }
+    }
+}
+
+struct CalendarUI_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarUI()
+    }
+}
---
+++ HHS MAD 2023/Preview Content/HomeScreen.swift
@@ -0,0 +1,49 @@
+//
+//  HomeScreen.swift
+//  HHS MAD 2023
+//
+//  Created by Julian NTH on 2/7/23.
+//
+
+import SwiftUI
+
+
+struct HomeScreen: View {
+    let screenRect = UIScreen.main.bounds
+    let width = UIScreen.main.bounds.size.width
+    let height = UIScreen.main.bounds.size.height
+    
+    var body: some View {
+        
+        GeometryReader{ geo in
+            
+            
+            ZStack{
+                Color(red: 231/255, green: 255/255, blue: 231/255)
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack{
+                    RoundedRectangle(cornerRadius: 100, style: .continuous)
+                        .fill(Color.white)
+                        .frame(width: width/2, height: height/4)
+                    HStack{
+                        
+                    }
+                }
+                
+//                Image("HHS_LOGO")
+//                    .renderingMode(.original)
+//                    .resizable()
+//                    .frame(width: width/1.4, height: height/4, alignment: .center)
+                    
+                
+            }
+        }
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
