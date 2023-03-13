ab094005027f34bfe8ecf24c381f4acac016f471
aaditya_raj <89165734+araj754@users.noreply.github.com>
2023-03-13 15:19:32 -0700

    commented out error

--- HHS MAD 2023/Preview Content/Upcoming Events.swift
+++ HHS MAD 2023/Preview Content/Upcoming Events.swift
@@ -224,23 +224,24 @@ func getDay(days: Int) -> DateComponents
 {
     // Get right now as it's `DateComponents`.
 
-    let now = Calendar.current.dateComponents(in: .current, from: Date())
-   
-
-    
-    // Create the start of the day in `DateComponents` by leaving off the time.
-    let today = DateComponents(year: now.year, month: now.month, day: now.day! + days)
-    let dateToday = Calendar.current.date(from: today)!
-    print(dateToday.timeIntervalSince1970)
-
-    // Add 1 to the day to get tomorrow.
-    // Don't worry about month and year wraps, the API handles that.
-    let tomorrow = DateComponents(year: now.year, month: now.month, day: now.day! + 1)
-    let dateTomorrow = Calendar.current.date(from: tomorrow)!
-   
-    
-
-    return today
+//    let now = Calendar.current.dateComponents(in: .current, from: Date())
+//
+//
+//
+//    // Create the start of the day in `DateComponents` by leaving off the time.
+//    let today = DateComponents(year: now.year, month: now.month, day: now.day! + days)
+//    let dateToday = Calendar.current.date(from: today)!
+//    print(dateToday.timeIntervalSince1970)
+//
+//    // Add 1 to the day to get tomorrow.
+//    // Don't worry about month and year wraps, the API handles that.
+//    let tomorrow = DateComponents(year: now.year, month: now.month, day: now.day! + 1)
+//    let dateTomorrow = Calendar.current.date(from: tomorrow)!
+//
+//
+//
+//    return today
+    return DateComponents();
 }
 
 //extension Date {
